dba3e73db0fbd678cae79e6102da190e48f3c90a
Mahim <mahimmarufuzzaman1234@gmail.com>
2025-05-23 22:30:10 -0400

    onBoarding screen finished

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function chat() {
+  return (
+    <SafeAreaView>
+      <Text>chat</Text>
+    </SafeAreaView>
+  );
+}
