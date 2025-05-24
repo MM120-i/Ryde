eaca99e000fb5cb22253df6d1b1025c2f2e8e47d
Mahim <mahimmarufuzzaman1234@gmail.com>
2025-05-23 21:28:18 -0400

    setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,10 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-lg my-10">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
   );
 }
